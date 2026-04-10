55ec429a61c8b115e679e49979837884cca7bea8
hahatulk <32220397+hahatulk@users.noreply.github.com>
2026-04-10 13:24:27 +0300

    Update 2.init_ssh.sh

--- module/2.init_ssh.sh
+++ module/2.init_ssh.sh
@@ -5,7 +5,7 @@ echo "Starting SSH config..."
 
 sudo rm -rf /etc/ssh/sshd_config.d/50-cloud-init.conf
 sudo cp sshd_config.d/10-custom.conf /etc/ssh/sshd_config.d/10.custom.conf
-
+sudo chmod 600 /etc/shadow
 sudo systemctl restart ssh
 
 echo "SSH config completed. Reboot recommended."
