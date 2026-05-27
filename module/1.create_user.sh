c64d49aa43947b5c36b9cb2569b362d78aacea3e
Vadim <goljatkin2128506@gmail.com>
2026-05-27 12:04:25 +0300

    fix

--- module/1.create_user.sh
+++ module/1.create_user.sh
@@ -4,56 +4,69 @@ echo "Starting User setup..."
 read -p "Enter the username to create: " username
 
 if ! id -u "$username" > /dev/null 2>&1; then
-	sudo useradd -m "$username"
-	sudo groupadd "$username"
-	sudo usermod -a -G "$username" "$username"
+  sudo useradd -m "$username"
+  sudo groupadd "$username"
+  sudo usermod -a -G "$username" "$username"
 
-	echo "Allow Sudo? (y/n):"
-	read allow_sudo
+  echo "Allow Sudo? (y/n):"
+  read allow_sudo
 
-	if [ "$allow_sudo" = "y" ]; then
-		echo "Allowed sudo"
-		sudo usermod -aG sudo "$username"
-		echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-	else
-		echo "Forbidden sudo"
-	fi
+  if [ "$allow_sudo" = "y" ]; then
+    sudo usermod -aG sudo "$username"
 
-	echo "Allow Docker? (y/n):"
-	read allow_docker
+    echo "Sudo without password? (y/n):"
+    read sudo_nopasswd
 
-	if [ "$allow_docker" = "y" ]; then
-		echo "Allowed docker"
-		sudo usermod -aG docker "$username"
-	else
-		echo "Forbidden docker"
-	fi
+    if [ "$sudo_nopasswd" = "y" ]; then
+      if ! grep -q "^$username ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+        echo "$username ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
+        echo "Sudo without password enabled"
+      else
+        echo "Sudo without password already configured"
+      fi
+    else
+      echo "Sudo with password enabled"
+    fi
+  else
+    echo "Sudo disabled"
+  fi
 
-	sudo mkdir -p /home/"$username"/.ssh
+  echo "Allow Docker? (y/n):"
+  read allow_docker
 
-	if [ ! -f /home/"$username"/.ssh/authorized_keys ]; then
-		sudo touch /home/"$username"/.ssh/authorized_keys
-	fi
+  if [ "$allow_docker" = "y" ]; then
+    echo "Allowed docker"
+    sudo usermod -aG docker "$username"
+  else
+    echo "Forbidden docker"
+  fi
 
-	echo "Add SSH KEY? (y/n):"
-	read add_ssh_key
+  sudo mkdir -p /home/"$username"/.ssh
 
-	if [ "$add_ssh_key" = "y" ]; then
-		echo "Enter ssh key: "
-		read ssh_key
-		sudo echo "$ssh_key" >> /home/"$username"/.ssh/authorized_keys
-	else
-		echo "Skipping SSH key..."
-	fi
+  if [ ! -f /home/"$username"/.ssh/authorized_keys ]; then
+    sudo touch /home/"$username"/.ssh/authorized_keys
+  fi
 
-	sudo usermod -s /bin/bash "$username"
+  echo "Add SSH KEY? (y/n):"
+  read add_ssh_key
+
+  if [ "$add_ssh_key" = "y" ]; then
+    echo "Enter ssh key: "
+    read ssh_key
+    sudo echo "$ssh_key" >> /home/"$username"/.ssh/authorized_keys
+  else
+    echo "Skipping SSH key..."
+  fi
+
+  sudo usermod -s /bin/bash "$username"
+
+  password=$(openssl rand -base64 16)
+  echo "$username:$password" | sudo chpasswd
+  echo "Password for user $username: $password"
 fi
 
 sudo chmod 700 /home/"$username"/.ssh
 sudo chmod 600 /home/"$username"/.ssh/authorized_keys
 sudo chown -R "$username":"$username" /home/"$username"/.ssh
-
-echo "Set password for user $username:"
-sudo passwd -u hahatulk
 
 echo "User setup completed."
