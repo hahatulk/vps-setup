de61edaed344479b79ea0319ae1fd57e5eed0eb4
hahatulk <32220397+hahatulk@users.noreply.github.com>
2026-06-25 02:03:07 +0300

    Update setup_vps.sh

--- setup_vps.sh
+++ setup_vps.sh
@@ -5,3 +5,6 @@ sudo bash ./module/1.create_user.sh
 sudo bash ./module/2.init_ssh.sh
 sudo bash ./module/3.init_ufw.sh
 #sudo bash ./module/4.init_deploy.sh
+
+sudo systemctl restart ssh
+sudo systemctl restart sshd
